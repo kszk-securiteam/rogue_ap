8a0594ffff48e99a1c958442747eaeec18d05008
balint355 <36678764+balint355@users.noreply.github.com>
2018-06-07 23:07:04 +0200

    add original capture script

---
+++ cap.py
@@ -0,0 +1,18 @@
+import sys
+import pyshark
+
+if len(sys.argv) == 1:
+	print('Add meg a fájlnevet!')
+else:
+	cap = pyshark.FileCapture(sys.argv[1])
+	tokens = ['password', 'pw', 'pass']
+
+	for i in cap:
+		try:
+			uri = i.http.get_field_value('request_full_uri')
+			data = i.http.get_field_value('file_data')
+			if any(token in data for token in tokens):
+				print('URL:', uri, '\nData:', data, '\n')
+		except:
+			pass
+	
